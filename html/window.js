ac4623c5ce506aff08612887273a772a39804052
djoamel <128717061+djoamel@users.noreply.github.com>
2024-04-04 15:55:18 +0100

    Add files via upload

---
+++ html/window.js
@@ -0,0 +1,49 @@
+var open=document.getElementById('open');
+var close=document.getElementById('close');
+var windw=document.getElementById('windw');
+
+function openk(){
+    windw.style.display = 'block';
+
+    
+}
+
+function closek(){
+    windw.style.display = 'none';
+   
+}
+
+window.onclick = function(event) {
+    if (event.target == windw) {
+      windw.style.display = "none";
+    }
+  }
+
+
+  window.onclick = function(event) {
+    if (event.target == windw1) {
+      windw1.style.display = "none";
+    }
+  }
+
+
+
+
+
+var open_1=document.getElementById('open1');
+var closem_1=document.getElementById('close1');
+var windw_1=document.getElementById('windw1');
+function openl(){
+    windw_1.style.display = 'block';
+    
+}
+
+function closel(){
+    windw_1.style.display = 'none';
+}
+
+
+
+
+  
+
